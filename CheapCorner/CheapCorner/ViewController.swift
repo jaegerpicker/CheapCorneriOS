d197d14c48d6352083393660fef9fc26ac9015a5
Shawn Campbell <jaegerpicker@gmail.com>
2014-06-22 22:48:29 -0400

    init

---
+++ CheapCorner/CheapCorner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheapCorner
+//
+//  Created by Shawn Campbell on 6/22/14.
+//  Copyright (c) 2014 Shawn Campbell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
